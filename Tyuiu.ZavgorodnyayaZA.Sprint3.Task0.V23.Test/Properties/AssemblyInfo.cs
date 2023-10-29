5fd2d949716bec38b907a756a92f6e242b22cab7
Rev090LT <mamontosaimon900@gmail.com>
2023-10-29 22:09:19 +0500

    Add project files.

---
+++ Tyuiu.ZavgorodnyayaZA.Sprint3.Task0.V23.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZavgorodnyayaZA.Sprint3.Task0.V23.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZavgorodnyayaZA.Sprint3.Task0.V23.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("85a52589-4a2d-4ccd-8f80-d0066f00590c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
