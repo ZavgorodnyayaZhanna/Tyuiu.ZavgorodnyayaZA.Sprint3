3780051433cc3d2bc8c61f970fec22706146233a
Rev090LT <mamontosaimon900@gmail.com>
2023-10-29 22:13:39 +0500

    Task1

---
+++ Tyuiu.ZavgorodnyayaZA.Sprint3.Task1.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.ZavgorodnyayaZA.Sprint3.Task1.V6.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuiu.ZavgorodnyayaZA.Sprint3.Task1.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            int value = 4;
+            int startValue = 1, stopValue = 10;
+            double res = dataService.GetSumSeries(value, startValue, stopValue), wait = -389_102.250;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ZavgorodnyayaZA.Sprint3.Task1.V6.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZavgorodnyayaZA.Sprint3.Task1.V6.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZavgorodnyayaZA.Sprint3.Task1.V6.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67f27aec-3069-4e4b-81b3-0da6387620f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
