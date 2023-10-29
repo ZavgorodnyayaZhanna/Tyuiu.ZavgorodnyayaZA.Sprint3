3f55d053feef7b968a825b40cd582c4a6b46bc5e
Rev090LT <mamontosaimon900@gmail.com>
2023-10-29 22:21:55 +0500

    Task3

---
+++ Tyuiu.ZavgorodnyayaZA.Sprint3.Task3.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZavgorodnyayaZA.Sprint3.Task3.V28.Lib;
+
+namespace Tyuiu.ZavgorodnyayaZA.Sprint3.Task3.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService dataService = new DataService();
+            string str = "f35hyt t4j 3gkg45";
+            char chr = 'r';
+            string res = dataService.ReplaceNumOnChar(str, chr), wait = "frrhyt trj rgkgrr";
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
